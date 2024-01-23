10e1d246602da9951fd02ec3efc91e36bd5f4542
DionneGomes <74210568+DionneGomes@users.noreply.github.com>
2024-01-23 15:57:05 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,156 @@
+// JavaScript Document
+console.log("Howdy!");
+
+var bodyElement = document.querySelector('body');
+var titelTekst = document.querySelector('#tekst');
+
+var ring1988 = document.querySelector('#ring1988');
+var ring1991 = document.querySelector('#ring1991');
+var ring1994 = document.querySelector('#ring1994');
+var ring1996 = document.querySelector('#ring1996');
+var ring1997 = document.querySelector('#ring1997');
+var ring1998 = document.querySelector('#ring1998');
+var ring1999 = document.querySelector('#ring1999');
+var ring2001 = document.querySelector('#ring2001');
+var ring2003 = document.querySelector('#ring2003');
+var ring2006 = document.querySelector('#ring2006');
+var ring2008 = document.querySelector('#ring2008');
+var ring2010 = document.querySelector('#ring2010');
+var ring2011 = document.querySelector('#ring2011');
+var ring2014 = document.querySelector('#ring2014');
+var ring2016 = document.querySelector('#ring2016');
+var ring2022 = document.querySelector('#ring2022');
+
+var sonicImg = document.querySelector('#sonicRoll');
+var sonicLink = document.querySelector('#link');
+
+function sonic1988() {
+    console.log('sonic 1988');
+    bodyElement.classList.add('background1988');
+    bodyElement.classList.remove('background2022', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1988.png';
+    sonicLink.href = "http://www.boredbro.com/play/adventure-games/sonic-black-white.html";
+}
+
+
+function sonic1991() {
+    bodyElement.classList.add('background1991');
+    bodyElement.classList.remove('background1988', 'background2022', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1991.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_(1991_video_game)";
+}
+
+function sonic1994() {
+    bodyElement.classList.add('background1994');
+    bodyElement.classList.remove('background1988', 'background1991', 'background2022', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1994.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=vZgRy64icNI";
+}
+
+function sonic1996() {
+    bodyElement.classList.add('background1996');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background2022', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1996.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_CD";
+}
+
+function sonic1997() {
+    bodyElement.classList.add('background1997');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background2022', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1997.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_3";
+}
+
+function sonic1998() {
+    bodyElement.classList.add('background1998');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background2022', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1998.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_3D_Blast";
+}
+
+function sonic1999() {
+    bodyElement.classList.add('background1999');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background2022', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic1999.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=UrYwWefW2eU";
+}
+
+function sonic2001() {
+    bodyElement.classList.add('background2001');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2022', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2001.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_Adventure";
+}
+
+function sonic2003() {
+    bodyElement.classList.add('background2003');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2022', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2003.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_Adventure_2";
+}
+
+function sonic2006() {
+    bodyElement.classList.add('background2006');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2022', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2006.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=ImOO7JUo1H0";
+}
+
+function sonic2008() {
+    bodyElement.classList.add('background2008');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2022', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2008.png';
+    sonicLink.href = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_(2006_video_game)";
+}
+
+function sonic2010() {
+    bodyElement.classList.add('background2010');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2022', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2010.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=H1zmB7VrhM4";
+}
+
+function sonic2011() {
+    bodyElement.classList.add('background2011');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2022', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2011.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=-kvlU5JZQrk";
+}
+
+function sonic2014() {
+    bodyElement.classList.add('background2014');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2022', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2014.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=L1SzeWaJa94";
+}
+
+function sonic2016() {
+    bodyElement.classList.add('background2016');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2022', 'body');
+    sonicImg.src = '../VIDii/images/sonic2016.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=ehapelrBHxQ";
+}
+
+function sonic2022() {
+    bodyElement.classList.add('background2022');
+    bodyElement.classList.remove('background1988', 'background1991', 'background1994', 'background1996', 'background1997', 'background1998', 'background1999', 'background2001', 'background2003', 'background2006', 'background2008', 'background2010', 'background2011', 'background2014', 'background2016', 'body');
+    sonicImg.src = '../VIDii/images/sonic2022.png';
+    sonicLink.href = "https://www.youtube.com/watch?v=47r8FXYZWNU";
+}
+
+ring1988.addEventListener('click', sonic1988);
+ring1991.addEventListener('click', sonic1991);
+ring1994.addEventListener('click', sonic1994);
+ring1996.addEventListener('click', sonic1996);
+ring1997.addEventListener('click', sonic1997);
+ring1998.addEventListener('click', sonic1998);
+ring1999.addEventListener('click', sonic1999);
+ring2001.addEventListener('click', sonic2001);
+ring2003.addEventListener('click', sonic2003);
+ring2006.addEventListener('click', sonic2006);
+ring2008.addEventListener('click', sonic2008);
+ring2010.addEventListener('click', sonic2010);
+ring2011.addEventListener('click', sonic2011);
+ring2014.addEventListener('click', sonic2014);
+ring2016.addEventListener('click', sonic2016);
+ring2022.addEventListener('click', sonic2022);
